6ee01b9c160a2b5393d6001641874bc399e57311
Sasa Tang <sasa.tang@student.american.edu>
2018-04-04 23:32:21 -0400

    Add new R file

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # mytest
 I don't really know what I am doing
 Try something new
+This is a local change
+Try again
---
+++ RforGit.R
@@ -0,0 +1,10 @@
+# This is just a R test
+
+library(tidyverse)
+dim(diamonds)
+names(diamonds)
+mean(diamonds$price) #3932.8
+
+devtools::install_github("SasaTang/mytest")
+file.exists("~/.ssh/id_rsa")
+
